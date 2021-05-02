d5cb5cf6b90cf2dc107ea24c6377e5717530f540
Manish Upadhyay <ermanishupadhyay@gmail.com>
2021-05-02 22:29:08 +0530

    Java project setup
    Java project setup

---
+++ src/VariableAndDataTypes.java
@@ -0,0 +1,17 @@
+
+public class VariableAndDataTypes {
+
+	public static void main(String[] args) {
+		
+		//What is variable - Variable is name of memory location to store values.
+		// we use variable to store data and use the data once it is required.
+		
+		//Data Type - Data type specifies the size and type of values that can be stored in an identifier.
+		// There are 2 data type is java
+		//1- Primitive Data Types: Byte, Short, int , long, float, double, boolean, char
+		//2- non-Primitive Data Types: String, Array, classes , interfaces
+		
+
+	}
+
+}
