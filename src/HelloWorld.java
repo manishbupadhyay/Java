d2cf90795d61f49b0733aafbe1daa0e8fe30ab05
Manish Upadhyay <ermanishupadhyay@gmail.com>
2021-06-19 17:31:50 +0530

    Sample java programs

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		System.out.print("Manish"); // this will print in same line
+		// to print output in next line we use println()
+		System.out.println("Upadhyay");
+	}
+
+}
