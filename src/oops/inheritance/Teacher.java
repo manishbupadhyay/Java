fb8c6837e56fc00244daaf00aa739dec77d6dd6d
Manish Upadhyay <ermanishupadhyay@gmail.com>
2021-08-15 07:26:45 -0400

    Inheritance - Hybrid inheritance
    Inheritance - Hybrid inheritance
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

--- src/oops/inheritance/Teacher.java
+++ src/oops/inheritance/Teacher.java
@@ -1,5 +1,11 @@
+/* 
+ * Hybrid inheritance
+ * Class Teacher inherit class Student and class Student inherit class School.
+ * Class NonTeachingStaff inherit class school
+ * From this class we can call school class method with creating of object of this class also we can call School class method as well
+ * Now to access method of NonTeachingStaff class, I've created object of NonTeachingStaff class in this class and I'm able to access method of NonTeachingStaff class
+ */
 package oops.inheritance;
-
 public class Teacher extends Student {
 	void teachSelenium() {
 		System.out.println("I am teaching selenium to students");
@@ -9,6 +15,10 @@ public class Teacher extends Student {
 		Teacher teacher = new Teacher();
 		teacher.teachSelenium();
 		teacher.mathClass();
+		javaClass();
+		
+		NonTeachingStaff nts = new NonTeachingStaff();
+		nts.workingInSchool();
 		javaClass();
 	}
 }
