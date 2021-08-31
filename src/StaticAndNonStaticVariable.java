6b7c944a23ae67bb220c87652a7c8510c3734db7
Manish Upadhyay <ermanishupadhyay@gmail.com>
2021-08-31 15:40:33 +0530

    Static and non-static variables and methods

--- src/StaticAndNonStaticVariable.java
+++ src/StaticAndNonStaticVariable.java
@@ -16,15 +16,21 @@ public class StaticAndNonStaticVariable {
 	
 	public static void main(String[] args) {
 		System.out.println("Hey, you're inside main method");
-		// we can call static method directly
+		// we can call static method directly also with class name
 		A();
-		// we can call static variable directly
+		StaticAndNonStaticVariable.A();
+		// we can call static variable directly also with class name
 		System.out.println("Hey, Value of a is: " + a);
+		System.out.println(StaticAndNonStaticVariable.a);
 		
 		// to call non-static method we need to create object of class
 		StaticAndNonStaticVariable obj1 = new StaticAndNonStaticVariable();
 		obj1.B();
 		// we can call no-static variable with object reference variable
 		System.out.println("Hey, value of d is:" +obj1.d);
+
+		// we can also call static methods and variables with object reference variable and we will get warning. In this case program will be run fine but this is not a good pratice
+		System.out.println(obj1.a);
+		obj1.A();
 	}
 }
